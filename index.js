2c38255e6e21485b18feed219af71c995be673b8
rokonroni <rokonuzzamanroni@gmail.com>
2024-02-01 17:58:15 +0600

    GET all categories API added

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const port = 3001;
 
 const db = new sqlite3.Database('./dua_main.sqlite');
 
+// Endpoint to fetch all categories
+app.get('/api/categories', (req, res) => {
+  db.all('SELECT * FROM category', (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json(rows);
+  });
+});
+
 
 
 // Start the server
