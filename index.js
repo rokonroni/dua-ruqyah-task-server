c68fef29d54fff8ad5ec436964de3712401215f0
rokonroni <rokonuzzamanroni@gmail.com>
2024-02-02 10:30:27 +0600

    Cors issue solve with cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const port = 3001;
+
+app.use(cors());
 
 const db = new sqlite3.Database('./dua_main.sqlite');
 
